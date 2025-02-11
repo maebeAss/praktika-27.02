f298b7e68480a51c1e421a4dc422277a229f23b0
maebeAss <gohanlice@mail.ru>
2025-02-11 14:04:54 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,172 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <h3>Mini apps</h3>
+          <div className='nav-bar-ul'>
+            <a>Почему Telegram?</a>
+            <a>Наши услуги</a>
+            <a>Преимущества</a>
+            <a>Как это работает</a>
+          </div>
+          <button className='nav-bar-butt'>
+            <p>Оставить заявку</p>
+          </button>
+        </nav>
+        <div className='hero-sec'>
+          <div className='hero-sec-main'>
+            <div className='hero-sec-text'>
+              <h1>Треть рынка в один клик!</h1>
+              <h4>Разработайте Telegram Mini App - будущее мобильной коммерции уже здесь!</h4>
+            </div>
+            <button className='hero-sec-butt'>
+              <p>Хочу мини-приложение!</p>
+            </button>
+          </div>
+          <section className='hero-img-sec'>
+            <video src='...'></video>
+          </section>
+        </div>
+      </header>
+      <main>
+        <div className='slider-sec'>
+          <button id='slider-nav-butt-left'>
+            <p>L</p>
+          </button>
+          <div className='main-slider'>
+            <div className='slider-navPoint'>
+              <div className='nav-point'></div>
+              <div className='nav-point'></div>
+              <div className='nav-point'></div>
+              <div className='nav-point'></div>
+            </div>
+            <h2>Real Estate</h2>
+            <div className='butt-sec'>
+              <button className='slider-butt'>
+                <p>Поиск</p>
+              </button>
+              <button className='slider-butt'>
+                <p>Выбор категории</p>
+              </button>
+            </div>
+            <div>
+              /* ТУТ СЛАЙДЕР РЕАЛИЗОВАТЬ */
+            </div>
+          </div>
+          <button id='slider-nav-butt-right'>
+            <p>R</p>
+          </button>
+        </div>
+        <div className='card-sec'>
+          <div className='card'>
+            <img></img>
+            <p>Цифровизация и автоматизация</p>
+            <p>управляйте продажами и коммуникацией</p>
+          </div>
+          <div className='card'>
+            <img></img>
+            <p>Цифровизация и автоматизация</p>
+            <p>управляйте продажами и коммуникацией</p>
+          </div>
+          <div className='card'>
+            <img></img>
+            <p>Цифровизация и автоматизация</p>
+            <p>управляйте продажами и коммуникацией</p>
+          </div>
+          <div className='card'>
+            <img></img>
+            <p>Цифровизация и автоматизация</p>
+            <p>управляйте продажами и коммуникацией</p>
+          </div>
+        </div>
+        <div>
+          <div className='card-img-sec'>
+            <img></img>
+          </div>
+          <div className='card-text-sec'>
+            <h2>Почему Telegram?</h2>
+            <ol>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ol>
+            <div className='card-sub-text'>
+              <p></p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div className='portfolio-sec'>
+          <div className='portf-content-sec'>
+            <div className='portf-text-sec'>
+              <h4>30+ экспертов с опытом работы в ИИ, машинном обучении и дизайне</h4>
+              <p>Полный цикл разработки: от анализа до запуска, а также гарантия качественного пользовательского опыта</p>
+            </div>
+            <div className='portf-sign-sec'>
+              <img></img>
+              <h4>Участник</h4>
+            </div>
+          </div>
+          <div className='portf-img-sec'>
+            <img></img>
+            <img></img>
+            <img></img>
+          </div>
+        </div>
+        <div className='howThatWorks-sec'>
+          <div className='title-sec'>
+            <h3>Как это работает?</h3>
+            <div className='miniCard-sec'>
+              <div className='card'>
+                <h4></h4>
+                <p></p>
+              </div>
+              <div className='card'>
+                <h4></h4>
+                <p></p>
+              </div>
+              <div className='card'>
+                <h4></h4>
+                <p></p>
+              </div>
+            </div>
+            <p></p>
+          </div>
+          <div className='map-sec'>
+            <img></img>
+          </div>
+        </div>
+        <div className='newLevel-sec'>
+          <div className='text-sec'>
+            <h2></h2>
+            <h4></h4>
+            <p></p>
+          </div>
+          <div className='forma-sec'>
+            <div className='nameForma-sec'>
+              <input></input>
+              <input></input>
+            </div>
+            <div className='phone+sphereForma-sec'>
+              <input></input>
+              <input></input>
+            </div>
+            <button>
+              <h4>Отправить</h4>
+            </button>
+            <p></p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
+
+/* FBI SIA KGD CNN IBM UCC YKK KKK TRF DOS TMN CCB ELE ELO REM MSG
+BBH DRI MDC SOD ALF BAF LFO YYO BBM SDP MCA MCAT UFO ESBB MBV VIE MTV LSD USA RIP */
